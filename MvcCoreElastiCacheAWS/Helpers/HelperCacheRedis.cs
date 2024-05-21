3b2a27ef0f0c9fd67a61fd9c7320810c8f591354
Anniiia <annaortegabueno@gmail.com>
2024-05-21 13:25:01 +0200

    commit

---
+++ MvcCoreElastiCacheAWS/Helpers/HelperCacheRedis.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+namespace MvcCoreElastiCacheAWS.Helpers
+{
+    public class HelperCacheRedis
+    {
+        private static Lazy<ConnectionMultiplexer> CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            //aqui iria nuestra cadena de conecion
+
+            return ConnectionMultiplexer.Connect("");
+        });
+
+        public ConnectionMultiplexer Connection
+        {
+            get 
+            {
+                return CreateConnection.Value;
+            }
+        }
+    }
+}
